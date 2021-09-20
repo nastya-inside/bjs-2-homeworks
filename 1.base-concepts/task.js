cda3181e68e349e4aa9ec1f40e67a82a341079b6
Anastasiya Grigorenko <you@example.com>
2021-09-20 17:39:13 +0400

    task.js changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,24 +2,25 @@
 
 function solveEquation(a, b, c) {
 let arr = [];
+let d;
     
   if ((a || a == 0) && (b || b == 0) && (c || c == 0)) {
     const discr = b**2-4*a*c;
     if (discr === 0) {
-        arr = -b/(2*a);
+        arr.push(-b/(2*a));
         console.log(arr);
     }
       else if (discr > 0) {
-          arr.push(-b + Math.sqrt(d))/(2*a);
+          arr.push((-b + Math.sqrt(discr))/(2*a));
           console.log(arr);
-          arr.push(-b - Math.sqrt(d))/(2*a);
+          arr.push((-b - Math.sqrt(discr))/(2*a));
           console.log(arr);
       }
-  }
+}
   return arr; // array
 }
 
-solveEquation();
+solveEquation(1, 12, 36);
 
 
 
