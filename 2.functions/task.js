4a5241ca20919a6800c66b821ae8a7a51153c34d
Anastasiya Grigorenko <you@example.com>
2021-10-01 21:24:02 +0400

    task.js was modified

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,50 +2,84 @@
 function getArrayParams(arr) {
     
   let min,max,sum,avg;
-  min = arr[0];
-  max = arr.length -1;
-  sum = max + min;
-  avg = sum / arr.length.toFixed();
-    
-    for (let i = 0; i > arr.lenght; i++) {
-        let element = arr[i];
-        if (element > max) {
-            element = max;
-        }
-        else if (element < min) {
-            element = min;   
-        }
-    }
+  min = Infinity;
+  max = -Infinity;
+  sum = 0; 
+  avg = Number((sum / arr.length).toFixed(2));
 
     
+    for (let i = 0; i < arr.length; i++) {
+        
+    let len = arr.length;
+        
+      if (arr[i] < min) {
+      min = arr[i];
+    }
+      else if (arr[i] > max) {
+      max = arr[i];
+    }
+        
+    sum += arr[i];
+    }
+
   return { min:min, max:max, avg:avg };
 }
 
 
-
-
-/*
-
-
 // Задание 2
+
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i++) {
+   sum += arr[i];
+ }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
+  let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max
+  for (let i = 0; i < arrOfArr.length; i++) {
+      let sum = func(arrOfArr[i]);
+      if (sum > max) {
+          max = func(arrOfArr[i]);
+      }
+      else {
+          return max;
+      }
+  }
 }
+
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+ let difference = 0; 
+  min = Infinity;
+  max = -Infinity;
+    
+
+  for (let i = 0; i < arr.length; i++) {    
+     if (arr[i] < min) {
+      min = arr[i];
+    }
+      else if (arr[i] > max) {
+      max = arr[i];
+    }
 }
+    
+  for (let i = 0; i < arr.length; i++) {
+      if (min > 0 && max > 0) {
+          difference = max - min;
+      }
+      else if (min < 0 && max > 0) {
+          difference = max - min;
+      }
+      else if (min > 0 && max < 0) {
+          difference = min - max; 
+      }
+      else if (min < 0 && max < 0) {
+         return difference = 0; 
+      }
+  }
+}
+
