84ab78509a7fa34e2d749d37c877b5c5a678a3bf
Anastasiya Grigorenko <you@example.com>
2021-10-14 17:40:19 +0400

    task.js was changed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 
 }
+
+const student1 = new Student("Ivan", "male", 21);
+const student2 = new Student("Olga", "female", 20);
+const student3 = new Student("Timur", "male", 18);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   this.subjectName = subjectName; 
 }
 
-// ваш код для остальных методов
+const subject1 = new setSubject("Math");
+const subject2 - new setSubject("PE");
+const subject3 = new setSubject("Art");
+
+
+Student.prototype.addMark = function (mark) {
+    this.mark = mark;
+    
+if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark); 
+    }   
+}
+
+const mark1 = new addMark(3);
+const mark2 = new addMark(4);
+const mark3 - new addMark(5); 
+
